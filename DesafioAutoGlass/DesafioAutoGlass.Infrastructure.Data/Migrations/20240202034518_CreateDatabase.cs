7bb83632060bc09dfcd430d2ed3484b245e62c3f
Marcos Nascimento <marcos.nascimento@confitec.com.br>
2024-02-02 00:48:56 -0300

    Criando banco de dados

---
+++ DesafioAutoGlass/DesafioAutoGlass.Infrastructure.Data/Migrations/20240202034518_CreateDatabase.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DesafioAutoGlass.Infrastructure.Data.Migrations
+{
+    public partial class CreateDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fornecedores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "varchar(200)", nullable: false),
+                    CNPJ = table.Column<string>(type: "varchar(14)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fornecedores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "varchar(200)", nullable: false),
+                    Status = table.Column<bool>(type: "Bit", nullable: false),
+                    ManufacturingDate = table.Column<DateTime>(type: "DateTime", nullable: true),
+                    ExpirationDate = table.Column<DateTime>(type: "DateTime", nullable: true),
+                    SupplierId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Produtos_Fornecedores_SupplierId",
+                        column: x => x.SupplierId,
+                        principalTable: "Fornecedores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_SupplierId",
+                table: "Produtos",
+                column: "SupplierId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+
+            migrationBuilder.DropTable(
+                name: "Fornecedores");
+        }
+    }
+}
